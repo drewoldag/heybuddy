11df6cd1f503cfa681fa04c5bb4408f0772fe86d
Drew Oldag <awoldag@uw.edu>
2023-05-24 14:58:29 -0700

    More playing with bokeh

--- src/heybuddy/main.py
+++ src/heybuddy/main.py
@@ -12,19 +12,20 @@ def hello_world(name):
 
 @app.route('/bokeh')
 def bokeh():
-    fig = figure(sizing_mode="stretch_width", height=600)
+    fig = figure(sizing_mode="stretch_width", height=600, x_axis_type="datetime")
+
     fig.step(
         x=[1,2,3,4],
-        y=[1.7, 2.2, 4.6, 3.9],
+        y=[1, 2.2, 4.6, 3.9],
         mode='after',
         color='navy',
         legend_label='Metric 1',
         line_width=2,
     )
+    fig.circle(x=[1,2,3,4],
+        y=[1, 2.2, 4.6, 3.9], legend_label='Metric 1', color='navy')
 
-    error_test = Whisker(base=2, upper=2.4, lower=2.0,
-                level="annotation", line_width=1, dimension="height", line_color='navy')
-
+    error_test = new_whisker(base=2, upper=2.4, lower=2.0)
     fig.add_layout(error_test)
 
     fig.step(
@@ -36,13 +37,13 @@ def bokeh():
         line_width=2
     )
 
-    error_test = Whisker(base=3, upper=0.7, lower=0.6,
-                level="annotation", line_width=1, dimension="height", line_color='red')
-    
+    fig.circle(x=[1,2,3,4,5],
+        y=[1.0, 0.8, 0.65, 0.5, 0.45], color='red', legend_label='Metric 2')
+
+    error_test = new_whisker(base=3, upper=0.7, lower=0.6)
     fig.add_layout(error_test)
 
-    test_span = Span(location=2.5, dimension='height', line_color='blue', line_width=1, line_dash='dashed', line_join='round')
-
+    test_span = Span(location=2.5, dimension='height', line_color='blue', line_width=1, line_dash='dashed')
     fig.add_layout(test_span)
 
     js_resources = INLINE.render_js()
@@ -58,5 +59,13 @@ def bokeh():
         css_resources=css_resources
     )
 
+def new_whisker(base, upper, lower):
+    w = Whisker(base=base, upper=upper, lower=lower,
+                level="annotation", line_width=1, dimension="height", line_color='black')
+    w.upper_head.size=4
+    w.lower_head.size=4
+
+    return w
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
